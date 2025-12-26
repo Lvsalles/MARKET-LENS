6dc06a1b0a59743a5402f013b491f73e89a81728
Lvsalles <livio.subscription@gmail.com>
2025-12-26 16:54:07 -0500

    Update schema_dictionary.py

--- schema_dictionary.py
+++ schema_dictionary.py
@@ -1,31 +1,23 @@
 SCHEMAS = {
     "residential": [
-        "id", "upload_id", "ml_number", "status",
-        "address", "city", "county", "zip",
-        "price", "area_sqft",
-        "beds", "baths", "year_built",
-        "agent_key", "agent_name",
+        "upload_id",
+        "ml_number",
+        "status",
+        "address",
+        "city",
+        "county",
+        "zip",
+        "price",
+        "area_sqft",
+        "beds",
+        "baths",
+        "year_built",
+        "agent_key",
+        "agent_name",
         "office_key",
         "buyer_financing",
         "property_type",
-        "adom", "cdom",
-        "created_at"
-    ],
-
-    "land": [
-        "id", "upload_id", "ml_number", "status",
-        "address", "city", "county", "zip",
-        "price", "area_sqft",
-        "agent_key",
-        "created_at"
-    ],
-
-    "rental": [
-        "id", "upload_id", "ml_number", "status",
-        "address", "city", "county", "zip",
-        "price", "area_sqft",
-        "beds", "baths",
-        "property_type",
-        "created_at"
+        "adom",
+        "cdom"
     ]
 }
