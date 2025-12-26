2e1fed461df9d96369eda862b1e7890d4c946b71
Lvsalles <livio.subscription@gmail.com>
2025-12-26 11:17:57 -0500

    Create semantic_dictionary.py

---
+++ semantic_dictionary.py
@@ -0,0 +1,26 @@
+SEMANTIC_DICTIONARY = {
+    "agent_key": [
+        "list_agent_id", "agent_id", "agentlicense", "license"
+    ],
+    "agent_name": [
+        "list_agent", "agent", "agent_full_name", "agentname"
+    ],
+    "office_key": [
+        "list_office_id", "office_id", "brokerage_id"
+    ],
+    "office_name": [
+        "list_office", "office", "brokerage"
+    ],
+    "price": [
+        "price", "list_price", "current_price", "close_price"
+    ],
+    "sqft": [
+        "sqft", "living_area", "heated_area", "building_area"
+    ],
+    "acreage": [
+        "acres", "acreage", "lot_acres"
+    ],
+    "zip": [
+        "zip", "zipcode", "postal_code"
+    ]
+}
