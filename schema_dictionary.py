e2c6a528703e18a72d06ed190feb430e699210c2
Lvsalles <livio.subscription@gmail.com>
2025-12-29 10:11:12 -0500

    Update schema_dictionary.py

--- schema_dictionary.py
+++ schema_dictionary.py
@@ -1,23 +1,11 @@
-SCHEMAS = {
-    "residential": [
-        "upload_id",
-        "ml_number",
-        "status",
-        "address",
-        "city",
-        "county",
-        "zip",
-        "price",
-        "area_sqft",
-        "beds",
-        "baths",
-        "year_built",
-        "agent_key",
-        "agent_name",
-        "office_key",
-        "buyer_financing",
-        "property_type",
-        "adom",
-        "cdom"
-    ]
-}
+CANONICAL_COLUMNS = [
+    "mls_id", "address", "city", "zipcode",
+    "category",
+    "property_type", "property_subtype", "financing",
+    "price", "sold_price", "sqft",
+    "beds", "baths", "garage",
+    "dom", "adom",
+    "list_date", "sold_date",
+    "ppsqft", "month_key",
+    "list_agent", "sell_agent",
+]
