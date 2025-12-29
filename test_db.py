75546c0f2f2887de3fec3f6b89d43930ee58463a
Lvsalles <livio.subscription@gmail.com>
2025-12-28 21:43:24 -0500

    Update test_db.py

--- test_db.py
+++ test_db.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine, text
-
-engine = create_engine("postgresql://postgres:G%40bys2010@db.xxxxx.supabase.co:5432/postgres")
+from db import engine
 
 with engine.connect() as conn:
-    print(conn.execute(text("SELECT current_database(), current_user")).fetchone())
+    result = conn.execute("SELECT now()")
+    print("CONNECTED:", result.fetchone())
